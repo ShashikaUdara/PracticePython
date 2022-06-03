ce527338a1b9fbe04102c47c4e643eff8c78f22d
ShashikaUdara <techdudelab@gmail.com>
2022-06-04 00:00:17 +0530

    garbage collection

--- Python OOP/Employee.py
+++ Python OOP/Employee.py
@@ -19,3 +19,17 @@ emp1.displayEmployee()
 emp2 = Employee("Amal", 2000)
 emp2.displayCount()
 emp2.displayEmployee()
+
+print("Employee.__doc__:", Employee.__doc__)
+print("Employee.__name__:", Employee.__name__)
+print("Employee.__module__:", Employee.__module__)
+print("Employee.__bases__:", Employee.__bases__)
+print("Employee.__dict__:", Employee.__dict__)
+
+# destroing objects (garbage collection)
+print(id(emp1))
+print(id(emp2))
+del emp1
+del emp2
+# print(id(emp1)) error because no such obj
+# print(id(emp2)) error because no such obj
